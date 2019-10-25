087015195702327724f7be766c004dd123b62ec6
usman <usman.solemn@gmail.com>
2019-10-25 21:48:47 +0500

    Image crop implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ImageCrop from './library/imageCrop';
 
-function App() {
+class App extends Component {
+
+    constructor(props, context) {
+    super(props, context);
+    this.state = {
+      userProfilePic: '',
+      editor: null,
+      scaleValue: 1,
+    };
+  }
+
+   setEditorRef = editor => this.setState({ editor });
+
+    onCrop = () => {
+    const { editor } = this.state;
+    if (editor !== null) {
+      const url = editor.getImageScaledToCanvas().toDataURL();
+      this.setState({ userProfilePic: url});
+    }
+  };
+
+  onScaleChange = (scaleChangeEvent) => {
+    const scaleValue =  parseFloat(scaleChangeEvent.target.value);
+    this.setState({ scaleValue });
+  };
+
+  DataURLtoFile = (dataurl, filename) => {
+  let arr = dataurl.split(','),
+    mime = arr[0].match(/:(.*?);/)[1],
+    bstr = atob(arr[1]),
+    n = bstr.length,
+    u8arr = new Uint8Array(n);
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new File([u8arr], filename, { type: mime });
+};
+
+ profilePicChange = (fileChangeEvent) => {
+    const file = fileChangeEvent.target.files[0];
+    const { type } = file;
+    if (!(type.endsWith('jpeg') || type.endsWith('png') || type.endsWith('jpg') || type.endsWith('gif'))) {
+    } else {
+      this.setState({ openCropper: true, selectedImage: fileChangeEvent.target.files[0], fileUploadErrors: [] });
+    }
+  };
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactk
-        </a>
-      </header>
+       <input type="file" name="profilePicBtn" accept="image/png, image/jpeg" onChange={this.profilePicChange} />
+       <ImageCrop
+          imageSrc={this.state.selectedImage}
+          setEditorRef={this.setEditorRef}
+          onCrop={this.onCrop}
+          scaleValue={this.state.scaleValue}
+          onScaleChange={this.onScaleChange}
+        />
+
+        <img src={this.state.userProfilePic} alt="Profile" />
     </div>
+
   );
+  }
 }
 
 export default App;
